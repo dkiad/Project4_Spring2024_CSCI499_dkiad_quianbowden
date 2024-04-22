dd6fb30c39599a1446eb47f1295d774678b07ba4
dkiad <122390190+dkiad@users.noreply.github.com>
2024-04-22 11:13:17 -0500

    Update Project5.php

--- Project5.php
+++ Project5.php
@@ -1,14 +1,54 @@
+<?php
+
+////// Write the Database connection code below (Q1)
+$servername = 'localhost'; //for XAMPP we use localhost
+   $username = 'root'; //default username in XAMPP
+   $password = ''; //default password in XAMPP
+   $dbname = 'project5'; //Change this to whatever database name you set in PHPmyAdmin
+
+   $link = mysqli_connect($servername,$username,$password, $dbname);
+if (mysqli_connect_error())
+{
+   die("Database connection is unsuccessful");
+
+}
+else 
+{
+   echo "Database connection is successful";
+};
+
+
+$queryInsert = "INSERT INTO users (email, password) VALUES ('dentdkia@gmail.com', 'yespassword')"; // this is used to update a row in the database. 
+
+
+mysqli_query($link, $queryInsert);
+
+$query = "SELECT * FROM users";
+
+if ($result = mysqli_query($link, $query)) {
+    while ($row = mysqli_fetch_array($result)) {
+        print_r($row);
+    }
+    mysqli_free_result($result); // Free result set
+}
+
+mysqli_close($link); // Close connection
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign Up - The Game Judge </title>
+    <title>The Game Judge: THE ARCADE  </title>
     <link rel="stylesheet" href="STYLE.css">
 </head>
-<body>
-    <div class="container">
-        <h1>Sign Up</h1>
+    <body>
+        <div class="container">
+            <h1>THE ARACADE </h1>
         <form action="signup.php" method="POST">
             <label for="username">Username:</label>
             <input type="text" id="username" name="username" required>
@@ -21,7 +61,9 @@
 
             <button type="submit">Sign Up</button>
         </form>
-        <p>Already have an account? <a href="login.html">Login here</a>.</p>
-    </div>
-</body>
+            <p>Already have an account? <a href="login.html">Login here</a>.</p>
+        </div>
+    </body>
+
+
 </html>
