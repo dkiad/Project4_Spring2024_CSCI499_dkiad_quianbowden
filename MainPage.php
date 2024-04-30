ddb1ed7694efc0403bc5d2e377fb51bd893c52fe
Quian Bowden <69886269+Qdboy@users.noreply.github.com>
2024-04-29 20:46:52 -0500

    Add files via upload

---
+++ MainPage.php
@@ -0,0 +1,18 @@
+<?php
+    require_once('Project5.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Game Judge: THE ARCADE  </title>
+    <link rel="stylesheet" href="STYLE.css">
+</head>
+
+<body>
+
+</body>
+</html>
